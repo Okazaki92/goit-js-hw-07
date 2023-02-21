7a135f8dc6fbd6b1b0b1aef7722658ceeaf8eee1
Okazaki92 <vincentslominski@gmail.com>
2023-02-21 18:49:24 +0100

    upd zad 1

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,31 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
-
 console.log(galleryItems);
+const galleryItem = galleryItems
+	.map(
+		({ preview, original, description }) =>
+			`<div class="gallery__item">
+  <a class="gallery__link" href="${original}">
+    <img
+      class="gallery__image"
+      src="${preview}"
+      data-source="${original}"
+      alt="${description}"
+    />
+  </a>
+</div>`,
+	)
+	.join("");
+const mainGallery = document.querySelector(".gallery");
+mainGallery.insertAdjacentHTML("beforeend", galleryItem);
+
+const imgZoom = (event) => {
+	event.preventDefault();
+	const img = event.target.dataset.source;
+	if (!img) return;
+	const instance = basicLightbox.create(`
+    <img src="${img}" width="800" height="600">
+`);
+	instance.show();
+};
+mainGallery.addEventListener("click", imgZoom);
